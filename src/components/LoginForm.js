2305cbb4897d8eaed80141d954aff7d3957a8857
AlexShom <a.shomalistos@gmail.com>
2020-01-09 18:27:32 +0000

    changed landing page

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,43 +5,24 @@ import {
   Grid,
   Header,
   Message,
-  Segment
+  Segment,
+  Divider,
+  GridColumn
 } from "semantic-ui-react";
 
 const LoginForm = () => (
-  <Grid textAlign="center" style={{ padding: "50px" }} verticalAlign="middle">
-    <Grid.Column style={{ maxWidth: 450 }}>
-        <Button size="huge" color="violet" inverted> Log in as a Guest </Button> 
-  <h2>or</h2>
-      <Form size="large" >
-        <Segment stacked >
-          <Header as="h2" color="teal" textAlign="center">
-            Log-in to your account
-          </Header>
-          <Form.Input
-            fluid
-            icon="user"
-            iconPosition="left"
-            placeholder="E-mail address"
-          />
-          <Form.Input
-            fluid
-            icon="lock"
-            iconPosition="left"
-            placeholder="Password"
-            type="password"
-          />
-
-          <Button color="teal" fluid size="large">
-            Login
-          </Button>
-        </Segment>
-      </Form>
-      <Message>
-        New to us? <a href="#">Sign Up</a>
-      </Message>
-    </Grid.Column>
-  </Grid>
+  <div style={{ padding: "20px" }}>
+    <Grid>
+      <Grid.Column>
+        <Button size="huge" color="violet" inverted>
+          Log in as a Guest
+        </Button>
+        <Button size="huge" color="blue" inverted>
+          Log in / Sign up
+        </Button>
+      </Grid.Column>
+    </Grid>
+  </div>
 );
 
 export default LoginForm;
