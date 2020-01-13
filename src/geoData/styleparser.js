21d93b045c47ee80b8ab6363bfc0ae1837d69364
AlexShom <a.shomalistos@gmail.com>
2020-01-13 09:40:40 +0000

    made postcode list from features

--- src/geoData/styleparser.js
+++ src/geoData/styleparser.js
@@ -1,13 +1,13 @@
 const jsonfile = require("jsonfile");
 const json = require("./nocollection.json");
 
-let counter = 0;
 
-json.forEach(element => {
-  let file = `/Users/alexshom/Documents/GitHub/relocate/relocate-frontend/src/geoData/individuals/${counter}.json`;
-  jsonfile.writeFile(file, element, function(err) {
-    if (err) console.error(err);
-    console.log(`writing file ${counter}`);
-    ++counter;
-  });
+const array = json.features.map(element => {
+  return element.properties.name
 });
+
+console.log(array)
+// const file = '/Users/alexshom/Documents/GitHub/relocate/relocate-frontend/src/geoData/averageRents.json'
+
+
+jsonfile.writeFile(file, array)
