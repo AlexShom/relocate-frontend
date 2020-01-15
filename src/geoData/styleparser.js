42afca61803b9eaf0141cfef1a3688a12d86d87f
AlexShom <a.shomalistos@gmail.com>
2020-01-15 06:43:10 +0000

    wrote script to parse out rental amounts

--- src/geoData/styleparser.js
+++ src/geoData/styleparser.js
@@ -1,47 +1,73 @@
 const jsonfile = require("jsonfile");
-// const list = require("./postcodeList.json");
-const fetch = require("node-fetch");
 
-// `https://api.propertydata.co.uk/rents?key=Y4U1F6RMFG&postcode=${code}&bedrooms=2`
+const mapData = require("./mapData.json");
+const beds0 = require("./individuals/0_beds_rent.json");
+const beds1 = require("./individuals/1_beds_rent.json");
+const beds2 = require("./individuals/2_beds_rent.json");
+const beds3 = require("./individuals/3_beds_rent.json");
+const beds4 = require("./individuals/4_beds_rent.json");
+const beds5 = require("./individuals/5_beds_rent.json");
+const rentals = require("./rentals.json")
 
-const getData = () => {
-  return new Promise((resolve, reject) => {
-    // const list = require("./postcodeList.json");
-    const list = ["E10", "E11"];
+// //
 
-    const promiseArr = list.map(i => {
-      return fetchData(i);
-    });
+// const bedsArray = [beds0, beds1, beds2, beds3, beds4, beds5]
 
-    resolve(Promise.all(promiseArr));
-  });
-};
+// const bedsRent = (featureCollection, bedsNum) => {
+//   array = featureCollection.map(resp => {
+//     if (resp.data) {
+//       return { [resp.postcode]: resp.data.long_let.average };
+//     } else {
+//       return null;
+//     }
+//   });
+//   return {[bedsNum]: cleaner(array)}
+// };
 
-const fetchData = code => {
-  const url = `https://api.propertydata.co.uk/rents?key=Y4U1F6RMFG&postcode=${code}&bedrooms=1`;
-  return new Promise(resolve => {
-    return fetch(url)
-      .then(resp => resp.json())
-      .then(data => {
-        console.log(`starting item ${data.postcode}`);
-        setTimeout(() => {
-          resolve(data);
-        }, 3000);
-      })
-      .catch(error => console.log(error));
-  });
-};
+// cleaner = (array) => {
+//   return array.filter(el => el !== null)
+// }
 
-const writeData = array => {
-  let file =
-    "/Users/alexshom/Documents/GitHub/relocate/relocate-frontend/src/geoData/individuals/1_beds_rent.json";
-  jsonfile.writeFile(file, array);
-};
+// //
 
-const runner = async () => {
-  const array = await getData();
-  console.log("writing to json:", array);
-  writeData(array);
-};
+// makeArray = (array) => {
+//   const newArray = array.map(collection => bedsRent(collection, array.indexOf(collection)))
+//   return (newArray)
+// }
 
-runner();
+// finalObj = makeArray(bedsArray)
+
+// jsonfile.writeFile("/Users/alexshom/Documents/GitHub/relocate/relocate-frontend/src/geoData/rentals.json", finalObj)
+
+
+const assignRents = (json, array, value) => {
+  
+  json.features.map(feature => {
+
+    array[value][value].map(pair => {
+      let key = Object.keys(pair)[0]
+      // console.log(pair[key])
+
+      if (feature.properties.name === key) {
+        feature.properties[`beds${value}Rent`] = pair[key]
+      }
+      if (!feature.properties[`beds${value}Rent`]) {
+        feature.properties[`beds${value}Rent`] = "FIND"
+      }
+
+    })
+  })
+}
+
+const runner = () => {
+  assignRents(mapData, rentals, 0)
+  assignRents(mapData, rentals, 1)
+  assignRents(mapData, rentals, 2)
+  assignRents(mapData, rentals, 3)
+  assignRents(mapData, rentals, 4)
+  assignRents(mapData, rentals, 5)
+  console.log(mapData.features.map(feature => feature.properties))
+  jsonfile.writeFile("/Users/alexshom/Documents/GitHub/relocate/relocate-frontend/src/geoData/mapWithRentals.json", mapData)
+}
+
+runner()
