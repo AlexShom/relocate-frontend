7fbc8d5cfafba4bd5a8ac7d6fa8f9dc04b5fe775
AlexShom <a.shomalistos@gmail.com>
2020-01-16 14:17:26 +0000

    moved state to dash for map styling, put in fetches for postcode info, put in pseudocode for filter if statement

---
+++ src/adapters/API.js
@@ -0,0 +1,2 @@
+export const postcodesAPI = "http://localhost:3000/postcodes"
+export const mapsAPI = "http://localhost:3000/maps?name=base_layer"
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Grid, Image, Container } from "semantic-ui-react";
+import { Grid, Container } from "semantic-ui-react";
 import Viewer from "../components/Viewer";
 import SearchCriteria from "../components/SearchCriteria";
+import { mapsAPI, postcodesAPI } from "../adapters/API";
 
 const Dashboard = props => {
   //Hooks
@@ -15,20 +16,50 @@ const Dashboard = props => {
 
   const [mapLayer, setMapLayer] = useState(null);
 
+  //Data driven styling
+
+  const [mapFilter, setMapFilter] = useState(["has", "name"]);
+  const [mapFillColor, setMapFillColor] = useState([
+    "match",
+    ["get", "name"],
+    "SW3",
+    "#fbb03b",
+    "#096925"
+  ]);
+
   //Helpers
 
   const getMapLayer = () => {
-    fetch("http://localhost:3000/maps/1")
+    fetch(mapsAPI)
       .then(resp => resp.json())
-      // .then(console.log)
+      .then(json => json.data)
       .then(json => setMapLayer(json));
+  };
+
+  const getPostcodeInfo = () => {
+    fetch(postcodesAPI)
+      .then(resp => resp.json())
+      // .then(resp)
+      .then(console.log);
   };
 
   //Lifecycle
 
   useEffect(() => {
     getMapLayer();
+    getPostcodeInfo();
   }, []);
+
+  useEffect(() => {
+    console.log("hit");
+    if (selectedFilter === "useRent") {
+      //runFilter(rentvalues)
+    } else if (selectedFilter === "usePrice") {
+      //runFilter(pricevalues)
+    } else {
+      //runfFilter(yieldvalues)
+    }
+  }, [selectedFilter]);
 
   //Render
 
@@ -60,7 +91,13 @@ const Dashboard = props => {
                 Map nav buttons
               </Container>
               <Container className="gen-box">
-                <Viewer mapLayer={mapLayer} />
+                <Viewer
+                  mapFillColor={mapFillColor}
+                  setMapFillColor={setMapFillColor}
+                  mapFilter={mapFilter}
+                  setMapFilter={setMapFilter}
+                  mapLayer={mapLayer}
+                />
               </Container>
             </Container>
           </Grid.Column>
