661587236046376ed60ae0b383d264620820bfe8
AlexShom <a.shomalistos@gmail.com>
2020-01-26 07:59:40 +0000

    fixed api

--- src/adapters/API.js
+++ src/adapters/API.js
@@ -1,6 +1,6 @@
 export const postcodesAPI = "https://relocate-backend.herokuapp.com/postcodes";
 export const mapsAPI =
-  "https://relocate-backend.herokuapp.com/maps&name=base_layer";
+  "https://relocate-backend.herokuapp.com/maps?name=base_layer";
 export const distanceAPI =
   "https://api.traveltimeapp.com/v4/geocoding/autocomplete";
 export const timeAPI =
